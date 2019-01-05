30fe1b945b55f66512d44a6b141e96c6e5333ae9
Ben Hofferber <hofferber.ben@gmail.com>
2019-01-04 17:23:06 -0700

    :cloud: Remove serverless and update ignoring sticky links
    Webtask is no longer supported by Serverless and thus we needed to migrate to simply using the Webtask CLI as it was simpler than switching to Lambda or another platform.

--- cron-dndmemes.js
+++ cron-dndmemes.js
@@ -3,56 +3,68 @@
 const request = require('request');
 const baseURL = 'https://bonebrawlers.slack.com/api/chat.postMessage';
 const defaultForm = {
-    token: 'oops',
-    channel: 'general',
-    as_user: false,
-    username: 'memebot',
-    icon_url: 'http://i.imgur.com/cM1OhgS.jpg',
-}
+  token: 'oops',
+  channel: 'general',
+  as_user: false,
+  username: 'memechest',
+  icon_url: 'https://media-waterdeep.cursecdn.com/avatars/thumbnails/0/211/1000/1000/636252764731637373.jpeg'
+};
 
 const callAPI = (form, cb) => {
-    request.post(baseURL, {form}, (err, res, body) => {
-        if (err) return cb(err);
-        body = JSON.parse(body);
-        if (!body.ok) return cb(body.error);
-        return cb(null, body);
-    });
+  console.log(form);
+  request.post(baseURL, { form }, (err, res, body) => {
+    if (err) return cb(err);
+    body = JSON.parse(body);
+    if (!body.ok) return cb(body.error);
+    return cb(null, body);
+  });
 };
 
 const getStoredValues = (context, cb) => {
-    context.storage.get((err, data) => {
-        if (err) return cb(err)
-        cb(data);
-    });
-}
+  context.storage.get((err, data) => {
+    if (err) return cb(err);
+    cb(data);
+  });
+};
 
 const setStoredValues = (context, data) => {
-    context.storage.set(data, (err) => {
-        if (err) console.log(err);
-    });
-}
+  context.storage.set(data, err => {
+    if (err) console.log(err);
+  });
+};
 
 module.exports = (context, cb) => {
-    request('https://www.reddit.com/r/dndmemes/.json?count=20', {json: true}, function(err, res, data) {
-        if (err) {
-            cb({ message: 'Error encountered', error: JSON.stringify(err) });
-        } else {
-            let post = data.data.children[0].data;
-            getStoredValues(context, (result) => {
-                // check to see if post was already posted
-                if (!result || result === post.url) {
-                    post = data.data.children[1].data;
-                }
-                callAPI(Object.assign({}, defaultForm, {
-                    text: `${post.title}
+  request('https://www.reddit.com/r/dndmemes/.json?count=20', { json: true }, function(err, res, data) {
+    if (err) {
+      cb({ message: 'Error encountered', error: JSON.stringify(err) });
+    } else {
+      let index = 0;
+      let post = data.data.children[index].data;
+      while (post.stickied) {
+        post = data.data.children[++index].data;
+      }
+      getStoredValues(context, result => {
+        console.log(result);
+        // check to see if post was already posted
+        if (!result || result === post.url) {
+          post = data.data.children[index + 1].data;
+        }
+        callAPI(
+          Object.assign({}, defaultForm, {
+            text: `${post.title}
 ${post.url}
 link: https://www.reddit.com${post.permalink}`
-                }), (err, body) => {
-                    if (err) { console.log(err); return cb(err); }
-                    cb(null, post);
-                    setStoredValues(context, post.url);
-                })
-            })
-        }
-    });
+          }),
+          (error, body) => {
+            if (error) {
+              console.log(err);
+              return cb(err);
+            }
+            cb(null, post);
+            setStoredValues(context, post.url);
+          }
+        );
+      });
+    }
+  });
 };
--- handler.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = (context, cb) => {
-    const message = 'Go Serverless & Webtasks! Your function executed successfully!';
-    console.log(message); // test logging
-    cb(null, { message });
-};
